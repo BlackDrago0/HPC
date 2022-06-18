4f5c55bd8cd96e5ddec7d9d3cb3d2363ee1b4656
BlackDrago0 <107365736+BlackDrago0@users.noreply.github.com>
2022-06-18 19:53:06 +0300

    Update and rename run to task.sh

---
+++ Lab2/task.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#PBS -l nodes=2:ppn=2
+#PBS -l walltime=0:02:00
+#PBS -N Mishyn_mpi
+
+cd /mnt/work/$USER
+
+ml icc
+ml openmpi
+mpirun -output-filename "./out/task_$arg1" $PBS_O_WORKDIR/hw
+
+if [[! -d "$PBS_O_WORKDIR/out"]]
+then
+mkdir $PBS_O_WORKDIR/out
+fi
+
+cp -R ./out/task_$arg1 $PBS_O_WORKDIR/out/task_$arg1
+
