3201845fca5ba0dbdca3a5ad2996ea203fc088ef
BlackDrago0 <107365736+BlackDrago0@users.noreply.github.com>
2022-06-20 20:49:21 +0300

    Create script.sh

---
+++ Lab4/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=24,walltime=00:50:00
+#PBS -N Mishyn_Lab4
+cd $PBS_O_WORKDIR
+ml icc
+ml openmpi
+
+for i in {1..24}
+do
+      mpirun -np $i ./m 1 20 $p_count
+done
